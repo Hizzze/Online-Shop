b91afbfe5c12ac79186e418a2eaf485214888169
cosmoqwe <cosmostar869@gmail.com>
2024-03-20 22:07:23 +0300

    test

---
+++ WebApplication2/Database.cs
@@ -0,0 +1,16 @@
+namespace WebApplication2;
+using Microsoft.Extensions.Configuration;
+public class Database
+{
+    private static string connectionString;
+
+    static Database()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        IConfigurationRoot config = builder.Build();
+        connectionString = config.GetConnectionString("DefaultConnection");
+    }
+    
+}

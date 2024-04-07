9f61a713d9bc6f510ca41a988e8d5fcaaca33546
Vlad Syzov <hizes5050@gmail.com>
2024-04-07 20:17:57 +0200

    cart v1

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b9c88b6479f415f86e02ce57b751304dc669ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41bb9013257c37f2a63601e956ea16db793ff5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
